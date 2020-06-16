1a4dda494be56b6075de58dc0bb49378e89a80e9
sebo <contact@sebastiansimko.sk>
2020-06-16 20:35:22 +0200

    add endpoint to list upload categories

--- modules/uploads/handler.go
+++ modules/uploads/handler.go
@@ -1,6 +1,34 @@
 package uploads
 
+import (
+	"base-site-api/modules"
+	"github.com/gofiber/fiber"
+	log "github.com/sirupsen/logrus"
+)
+
 // Handler for the uploads
 type Handler struct {
-	s Service
+	modules.Handler
+	service Service
+}
+
+func NewHandler(s Service) *Handler {
+	return &Handler{
+		service: s,
+	}
+}
+
+func (h *Handler) ListCategories(c *fiber.Ctx) {
+	t := c.Params("type")
+
+	categories, err := h.service.UploadCategories(t)
+
+	if err != nil {
+		log.Debugf("Error while getting pages by category slug %s", err)
+		h.Error(c, 404)
+
+		return
+	}
+
+	h.JSON(c, 200, categories)
 }
--- modules/uploads/uploads.go
+++ modules/uploads/uploads.go
@@ -1,1 +1,26 @@
 package uploads
+
+import (
+	"base-site-api/config"
+	"base-site-api/middleware/auth"
+	"github.com/gofiber/fiber"
+)
+
+// New prepare whole module and connect it with App
+func New(config *config.Config, api *fiber.Group) {
+	handler := NewHandler(NewService(NewRepository(config.Database)))
+
+	pages := api.Group("/v1/uploads")
+	pages.Use(auth.New(&auth.Config{
+		SigningKey: config.SigningKey,
+		Filter:     auth.FilterGetOnly,
+	}))
+
+	pages.Get("/:type/", handler.ListCategories)
+	//pages.Get("/:page-category", handler.ListPages)
+	//pages.Get("/:slug", handler.GetDetail)
+	//pages.Post("/:page-category", handler.Create)
+	//pages.Put("/:id", handler.Update)
+	//pages.Delete("/:id", handler.Remove)
+
+}
